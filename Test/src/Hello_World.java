b4cd13d3353d9f751e8fb279c233f0edb3d2b6f6
Pascal <pascal.kiessler@haw-hamburg.de>
2020-03-24 13:03:24 +0100

    Methode implementiert

---
+++ Test/src/Hello_World.java
@@ -0,0 +1,9 @@
+public class Hello_World {
+    public static void main (String [] args){
+        String test = "Hello World";
+        int count = 100;
+        for (int i = 1; i <= count; i++){
+            System.out.println(i + ": " + test);
+        }
+    }
+}
