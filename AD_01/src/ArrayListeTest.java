08032d41ca207a2d6def780b4abf52fda6f4c76e
Pascal <pascal.kiessler@haw-hamburg.de>
2020-04-05 10:53:16 +0200

    Bugfixes, Testklassen

---
+++ AD_01/src/ArrayListeTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListeTest {
+
+    @org.junit.jupiter.api.Test
+    void anzahlElemente() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void einfuegen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void entfernen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void gibElement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void leere() {
+    }
+}
