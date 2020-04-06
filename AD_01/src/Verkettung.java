b283bba1a9fbb2de987435090f9ead0fe83941b7
Pascal <pascal.kiessler@haw-hamburg.de>
2020-04-07 01:39:26 +0200

    Ich habe die Testklasse geschrieben und den Code der Verkettungsklasse etwas geändert. Er ist etwas länger geworden, aber schneller in der Ausführung.

--- AD_01/src/Verkettung.java
+++ AD_01/src/Verkettung.java
@@ -1,8 +1,11 @@
-public class Verkettung<T> implements LineareListe {
-    private DoppelKnoten _kopf;
-    private DoppelKnoten _ende;
-    private int _anzahlElemente;    //Kardinalitaet
+public class Verkettung <T> implements LineareListe <T>{
+    private DoppelKnoten _kopf;     //Wächter für den Listenanfang
+    private DoppelKnoten _ende;     //Wächter für das Listenende
+    private int _anzahlElemente;    //Kardinalitaet der Liste
 
+    /**
+     * Konstruktor der Klasse Verkettung
+     */
     public Verkettung(){
         _anzahlElemente = 0;
         _kopf = new DoppelKnoten();
@@ -11,58 +14,128 @@ public class Verkettung<T> implements LineareListe {
         _ende.setzeVorgaenger(_kopf);
     }
 
+    /**
+     * Liefert die aktuelle Anzahl an Elementen wieder zurück, die sich in der liste befinden.
+     * @return
+     */
     @Override
     public int anzahlElemente() {
         return _anzahlElemente;
     }
 
-    //Warum Object
+    /**
+     * Einfügen eines Doppelknotens in die Liste.
+     * Man kann die Stelle Wöhlen, an die das neue Objekt eingefügt werden soll.
+     * @param position
+     * @param element
+     * @throws IllegalArgumentException
+     */
     @Override
     public void einfuegen(int position, Object element) throws IllegalArgumentException {
-        if((position >= 0) && (position <= _anzahlElemente))
+        darfNichtNullSein(element);
+        mussGueltigeEinfuegepositionSein(position);
+
+        DoppelKnoten neuesKettenglied = new DoppelKnoten();
+        DoppelKnoten aktuelleStelle;
+        neuesKettenglied.setzeElement(element);
+        if (position < _anzahlElemente / 2)
         {
-            DoppelKnoten rechts = knotenAnPosition(position);
-            DoppelKnoten links = rechts.gibVorgaenger();
-            DoppelKnoten neu = new DoppelKnoten(links, rechts, element);
-            links.setzeNachfolger(neu);
-            rechts.setzeVorgaenger(neu);
-            _anzahlElemente++;
+            /*
+             * Wenn das Element in der ersten Hälfte der Liste eingefügt werden soll,
+             * wird die Liste von dem Anfang durchgegangen.
+             */
+            aktuelleStelle = _kopf;
+            int durchlaufen = position + 1;
+            for (int i = 0;i < durchlaufen; i++)
+            {
+                aktuelleStelle = aktuelleStelle.gibNachfolger();
+            }
         }
         else
         {
-            throw new IllegalArgumentException("Ungueltige Position");
+            /*
+             * Wenn das Element weiter hinten in der Liste eingefügt werden soll,
+             * wird vom Ende aus durchgegangen, um Rechenleistung zu sparen.
+             */
+            aktuelleStelle = _ende;
+            int durchlaufen = _anzahlElemente - position;
+            for (int i = 0; i < durchlaufen; i++)
+            {
+                aktuelleStelle = aktuelleStelle.gibVorgaenger();
+            }
         }
+        DoppelKnoten vorgaenger = aktuelleStelle.gibVorgaenger();
+        vorgaenger.setzeNachfolger (neuesKettenglied);
+        neuesKettenglied.setzeVorgaenger(vorgaenger);
+        neuesKettenglied.setzeNachfolger(aktuelleStelle);
+        aktuelleStelle.setzeVorgaenger(neuesKettenglied);
+        _anzahlElemente++;
     }
 
+    /**
+     * Entfernt ein Element aus der Liste und verkettet die Liste wieder richtig.
+     * Die Position des Elementes kann frei gewählt werden.
+     * @param position
+     * @throws IllegalArgumentException
+     */
     @Override
     public void entfernen(int position) throws IllegalArgumentException {
-        if((position >= 0) && (position <= _anzahlElemente))
+        mussGueltigePositionSein(position);
+        DoppelKnoten aktuelleStelle;
+        if (position < _anzahlElemente / 2)     //#Bestimmt die aktuelle Stelle
         {
-            DoppelKnoten links = knotenAnPosition(position).gibVorgaenger();
-            DoppelKnoten rechts = knotenAnPosition(position).gibNachfolger();
-            links.setzeNachfolger(rechts);
-            rechts.setzeVorgaenger(links);
-            _anzahlElemente--;
+            /*
+             * Wenn ein Element in der ersten hälfte gelöscht werden soll, dann geht die Liste
+             * von Anfang an alles durch um Rechenleistung zu sparen
+             */
+            aktuelleStelle = _kopf;
+            int durchlaufen = position + 1;
+            for (int i = 0;i < durchlaufen; i++)
+            {
+                aktuelleStelle = aktuelleStelle.gibNachfolger();
+            }
         }
         else
         {
-            throw new IllegalArgumentException("Ungueltige Position");
+            /*
+             * Wenn ein Element in der zweiten hälfte gelöscht werden soll, dann geht die Liste
+             * vom Ende an alles rückwärts durch, um Rechenleistung zu sparen.
+             */
+            aktuelleStelle = _ende;
+            int durchlaufen = _anzahlElemente - position;
+            for (int i = 0; i < durchlaufen; i++)
+            {
+                aktuelleStelle = aktuelleStelle.gibVorgaenger();
+            }
         }
-
+        DoppelKnoten vorgaenger = aktuelleStelle.gibVorgaenger();
+        DoppelKnoten nachfolger = aktuelleStelle.gibNachfolger();
+        vorgaenger.setzeNachfolger(nachfolger);
+        nachfolger.setzeVorgaenger(vorgaenger);
+        _anzahlElemente--;
     }
 
+    /**
+     * Liefert das Element an einer bestimmten Position wieder zurück.
+     * @param position
+     * @return
+     * @throws IllegalArgumentException
+     */
     @Override
-    public Object gibElement(int position) throws IllegalArgumentException {
-        if((position >= 0) && (position <= _anzahlElemente))
+    public T gibElement(int position) throws IllegalArgumentException {
+        mussGueltigePositionSein(position);
+        DoppelKnoten knoten = _kopf;
+        for (int i = 0; i <= position; ++i)
         {
-            return knotenAnPosition(position).gibElement();
+            knoten = knoten.gibNachfolger();
         }
-        else
-        {
-            throw new IllegalArgumentException("Ungueltige Position");
-        }
+        return (T) knoten.gibElement();
     }
 
+    /**
+     * Leert die komplette Liste, indem nur die Wächter wieder verbunden werden und der
+     * garbage collector die Elemente entfernt.
+     */
     @Override
     public void leere() {
         _ende.setzeVorgaenger(_kopf);
@@ -70,7 +143,11 @@ public class Verkettung<T> implements LineareListe {
         _anzahlElemente = 0;
     }
 
-    // Ueberpruefen
+    /**
+     *
+     * @param position
+     * @return
+     */
     private DoppelKnoten knotenAnPosition(int position)
     {
         DoppelKnoten result;
@@ -85,6 +162,11 @@ public class Verkettung<T> implements LineareListe {
         return result;
     }
 
+    /**
+     *
+     * @param position
+     * @return
+     */
     private DoppelKnoten knotenAnPositionAufsteigend(int position)
     {
         DoppelKnoten knoten = _kopf;
@@ -95,6 +177,11 @@ public class Verkettung<T> implements LineareListe {
         return knoten;
     }
 
+    /**
+     *
+     * @param position
+     * @return
+     */
     private DoppelKnoten knotenAnPositionAbsteigend(int position)
     {
         DoppelKnoten knoten = _ende;
@@ -103,5 +190,60 @@ public class Verkettung<T> implements LineareListe {
             knoten = knoten.gibVorgaenger();
         }
         return knoten;
+    }
+
+    /* -------------------------------------------------------------------------------------------------------------*/
+
+
+    /**
+     * Liefert true fuer alle gueltigen Positionen innerhalb der Liste.
+     */
+    public boolean istGueltigePosition(int position)
+    {
+        return (position >= 0) && (position < anzahlElemente());
+    }
+
+    /**
+     * Wirft eine IndexOutOfBoundsException, falls es sich um eine ungueltige
+     * Position handelt.
+     */
+    private void mussGueltigePositionSein(int position)
+    {
+        if (!istGueltigePosition(position))
+        {
+            throw new IndexOutOfBoundsException(position + " ist keine gueltige Position");
+        }
+    }
+
+    /**
+     * Liefert true fuer alle gueltigen Einfuegepositionen innerhalb der Liste.
+     */
+    public boolean istGueltigeEinfuegeposition(int position)
+    {
+        return (position >= 0) && (position <= anzahlElemente());
+    }
+
+    /**
+     * Wirft eine IndexOutOfBoundsException, falls es sich um eine ungueltige
+     * Einfuegeposition handelt.
+     */
+    private void mussGueltigeEinfuegepositionSein(int position)
+    {
+        if (!istGueltigeEinfuegeposition(position))
+        {
+            throw new IndexOutOfBoundsException(
+                    position + " ist keine gueltige Einfuegeposition");
+        }
+    }
+
+    /**
+     * Wirft eine IllegalArgumentException, falls die uebergebene Titel-Referenz null ist.
+     */
+    private static void darfNichtNullSein(Object element)
+    {
+        if (element == null)
+        {
+            throw new IllegalArgumentException("Das Element darf nicht Null sein!");
+        }
     }
 }
---
+++ AD_01/src/VerkettungTest.java
@@ -0,0 +1,198 @@
+import org.junit.jupiter.api.Test;
+/**
+ * @author Santa Rudevica
+ * @author Daniel Dichte
+ * @author Pascal Kießler
+ */
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+
+class VerkettungTest {
+    /* ---------- Instanzvariablen ---------- */
+    private DoppelKnoten _elemente;
+    private Integer testArray [];
+    private final int anzahlStellenArray;
+
+    /* ---------- Konstruktor ---------- */
+    public VerkettungTest () {
+        anzahlStellenArray = 100;
+        _elemente = new DoppelKnoten();
+        testArray = new Integer [anzahlStellenArray];
+    }
+
+    /**
+     * Testet die aktuelle Anzahl der Elemente die sich in dem Array befinden.
+     */
+    @org.junit.jupiter.api.Test
+    void testAnzahlElemente() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            liste.einfuegen(0, testArray[i]);
+        }
+        assertEquals(100, liste.anzahlElemente());
+    }
+
+
+    /**
+     * Testet das einfügen von Elementen in die Arrayliste.
+     * Alle Elemente werden an die erste Stelle im Array geschrieben.
+     */
+    @org.junit.jupiter.api.Test
+    void testEinfuegenErsteStelle() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            liste.einfuegen(0, testArray[i]);
+        }
+        assertEquals(100, liste.anzahlElemente());
+        assertEquals(42, liste.gibElement((anzahlStellenArray / 2)));
+    }
+
+
+    /**
+     * Testet ob man irgendwo in der liste einfügen kann
+     */
+    @org.junit.jupiter.api.Test
+    void testEinfuegenMitte() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+        int letzteStelle = 0;
+        Integer letzterWert = 0;
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            letzteStelle = randomWertFuerStelle(i);
+            liste.einfuegen(letzteStelle, testArray[i]);
+            letzterWert = testArray [i];
+        }
+        assertEquals(100, liste.anzahlElemente());
+        assertEquals(letzterWert, liste.gibElement(letzteStelle));
+    }
+
+
+    /**
+     * Testet ob man weiterhin immer am ende in der Liste einfügen kann.
+     */
+    @org.junit.jupiter.api.Test
+    void testEinfuegenLetzteStelle() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            liste.einfuegen(i, testArray[i]);
+        }
+        assertEquals(100, liste.anzahlElemente());
+        assertEquals(42, liste.gibElement((anzahlStellenArray / 2) - 1));
+    }
+
+
+    /**
+     * Testet das entfernen an der ersten Stelle des Arrays
+     */
+    @org.junit.jupiter.api.Test
+    void testEntfernenAnfang() {
+        LineareListe liste = erzeugeListe();
+        fuelleTestArray();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){  //zum erstellen der Liste
+            liste.einfuegen(0, testArray[i]);
+        }
+        for (int i = 0; i <= 9; i++) {  // zum löschen von 10 elementen
+            liste.entfernen(0);
+        }
+        assertEquals(90, liste.anzahlElemente());
+    }
+
+    /**
+     * Testet das entfernen eines Elementes am Ende der Schleife.
+     */
+    @org.junit.jupiter.api.Test
+    void testEntfernenEnde() {
+        LineareListe liste = erzeugeListe();
+        fuelleTestArray();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){ // zum erstellen der Liste
+            liste.einfuegen(0, testArray[i]);
+        }
+        for (int i = 99; i >= 90; i--) {  //zum löschen von 10 elementen
+            liste.entfernen(i);
+        }
+        assertEquals(90, liste.anzahlElemente());
+    }
+
+
+    /**
+     * Testet das Rückgabeelement von der übergebenen Position
+     */
+    @org.junit.jupiter.api.Test
+    void testGibElement() {
+        LineareListe liste = erzeugeListe();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){ // zum erstellen der Liste
+            liste.einfuegen(i, i);
+        }
+        assertEquals(0, liste.gibElement(0));
+        assertEquals(1, liste.gibElement(1));
+        assertEquals(98, liste.gibElement(98));
+        assertEquals(99, liste.gibElement(99));
+    }
+
+
+    /**
+     * Testet das leeren des gesamten Arrays
+     */
+    @org.junit.jupiter.api.Test
+    void testLeere() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){  //zum erstellen der Liste
+            liste.einfuegen(0, testArray[i]);
+        }
+        liste.leere();
+        assertEquals(0, liste.anzahlElemente());
+    }
+
+
+    /**
+     * Hilfsmethode um einen zufällig generierten Integer Wert zu erzeugen
+     * @return
+     */
+    Integer randomWertFuerArray() {
+        int bisStelle = 2147483647;
+        Random intWert = new Random();
+        int rueckgabeInteger = intWert.nextInt(bisStelle);
+        return rueckgabeInteger;
+    }
+
+    /**
+     * Hilfsmethode um einen Zufallswert für
+     * @param stelle
+     * @return
+     */
+    Integer randomWertFuerStelle (int stelle) {
+        Random intWert = new Random();
+        int rueckgabeInteger = intWert.nextInt(1 + stelle);
+        return rueckgabeInteger;
+    }
+
+    /**
+     * Hilfsmethode um das Testarray zu füllen
+     */
+    public void fuelleTestArray () {
+        Integer testInteger;
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            if (i == anzahlStellenArray / 2 - 1) {
+                testArray [i] = 42;
+            } else {
+                testArray [i] = randomWertFuerArray();
+            }
+        }
+    }
+
+    /**
+     * Methode um ein Objekt zu erzeugen der Liste.
+     * @return
+     */
+    public LineareListe erzeugeListe() {
+        return new Verkettung();
+    }
+
+
+}
