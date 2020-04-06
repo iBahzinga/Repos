879b5dd75007709392f2418e65a3cc3bdadbfe45
Pascal <pascal.kiessler@haw-hamburg.de>
2020-04-06 17:40:25 +0200

    Ich habe die Testklasse geschrieben und den Code der Verkettungsklasse etwas geändert. Er ist etwas länger geworden, aber schneller in der Ausführung.

--- AD_01/src/VerkettungTest.java
+++ AD_01/src/VerkettungTest.java
@@ -1,26 +1,172 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
 
 class VerkettungTest {
+    /* ---------- Instanzvariablen ---------- */
+    private DoppelKnoten _elemente;
+    private Integer testArray [];
+    private final int anzahlStellenArray;
 
-    @Test
-    void anzahlElemente() {
+    /* ---------- Konstruktor ---------- */
+    public VerkettungTest () {
+        anzahlStellenArray = 100;
+        _elemente = new DoppelKnoten();
+        testArray = new Integer [anzahlStellenArray];
     }
 
-    @Test
-    void einfuegen() {
+    /**
+     * Testet die aktuelle Anzahl der Elemente die sich in dem Array befinden.
+     */
+    @org.junit.jupiter.api.Test
+    void testAnzahlElemente() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            liste.einfuegen(0, testArray[i]);
+        }
+        assertEquals(100, liste.anzahlElemente());
     }
 
-    @Test
-    void entfernen() {
+
+    /**
+     * Testet das einfügen von Elementen in die Arrayliste.
+     * Alle Elemente werden an die erste Stelle im Array geschrieben.
+     */
+    @org.junit.jupiter.api.Test
+    void testEinfuegenErsteStelle() { /**Dieser Test ist noch nicht ganz richtig*/
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            liste.einfuegen(0, testArray[i]);
+        }
+        assertEquals(100, liste.anzahlElemente());
+        assertEquals(42, liste.gibElement((anzahlStellenArray / 2) - 1));
     }
 
-    @Test
-    void gibElement() {
+    @org.junit.jupiter.api.Test
+    void testEinfuegenMitte() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+        int letzteStelle = 0;
+        Integer letzterWert = 0;
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            letzteStelle = randomWertFuerStelle(i);
+            liste.einfuegen(letzteStelle, testArray[i]);
+            letzterWert = testArray [i];
+        }
+        assertEquals(100, liste.anzahlElemente());
+        assertEquals(letzterWert, liste.gibElement(letzteStelle));
     }
 
-    @Test
-    void leere() {
+
+    @org.junit.jupiter.api.Test
+    void testEinfuegenLetzteStelle() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            liste.einfuegen(i, testArray[i]);
+        }
+        assertEquals(100, liste.anzahlElemente());
+        assertEquals(42, liste.gibElement((anzahlStellenArray / 2) - 1));
     }
+
+
+    /**
+     * Testet das entfernen an der ersten Stelle des Arrays
+     */
+    @org.junit.jupiter.api.Test
+    void testEntfernenAnfang() {
+        LineareListe liste = erzeugeListe();
+        fuelleTestArray();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){  //zum erstellen der Liste
+            liste.einfuegen(0, testArray[i]);
+        }
+        for (int i = 0; i <= 9; i++) {  // zum löschen von 10 elementen
+            liste.entfernen(0);
+        }
+        assertEquals(90, liste.anzahlElemente());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEntfernenEnde() {  /** Dieser Test ist noch nicht ganz richtig */
+        LineareListe liste = erzeugeListe();
+        fuelleTestArray();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){ // zum erstellen der Liste
+            liste.einfuegen(0, testArray[i]);
+        }
+        for (int i = 99; i <= 90; i--) {  //zum löschen von 10 elementen
+            liste.entfernen(i);
+        }
+        assertEquals(90, liste.anzahlElemente());
+    }
+
+
+    /**
+     * Testet das Rückgabeelement von der übergebenen Position
+     */
+    @org.junit.jupiter.api.Test
+    void testGibElement() {
+        LineareListe liste = erzeugeListe();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){ // zum erstellen der Liste
+            liste.einfuegen(i, i);
+        }
+        assertEquals(0, liste.gibElement(0));
+        assertEquals(1, liste.gibElement(1));
+        assertEquals(98, liste.gibElement(98));
+        assertEquals(99, liste.gibElement(99));
+    }
+
+
+    /**
+     * Testet das leeren des gesamten Arrays
+     */
+    @org.junit.jupiter.api.Test
+    void testLeere() {
+        fuelleTestArray();
+        LineareListe liste = erzeugeListe();
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){  //zum erstellen der Liste
+            liste.einfuegen(0, testArray[i]);
+        }
+        liste.leere();
+        assertEquals(0, liste.anzahlElemente());
+    }
+
+
+    /**
+     * Hilfsmethode um einen zufällig generierten Integer Wert zu erzeugen
+     * @return
+     */
+    Integer randomWertFuerArray() {
+        int bisStelle = 2147483647;
+        Random intWert = new Random();
+        int rueckgabeInteger = intWert.nextInt(bisStelle);
+        return rueckgabeInteger;
+    }
+
+    Integer randomWertFuerStelle (int stelle) {
+        Random intWert = new Random();
+        int rueckgabeInteger = intWert.nextInt(1 + stelle);
+        return rueckgabeInteger;
+    }
+
+    public void fuelleTestArray () {
+        Integer testInteger;
+        for (int i = 0; i <= anzahlStellenArray - 1; i++){
+            if (i == anzahlStellenArray / 2 - 1) {
+                testArray [i] = 42;
+            } else {
+                testArray [i] = randomWertFuerArray();
+            }
+        }
+    }
+
+    public LineareListe erzeugeListe() {
+        return new Verkettung();
+    }
+
+
 }
