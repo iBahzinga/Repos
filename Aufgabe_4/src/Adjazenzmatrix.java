9056b5ba247c60d20325e6b824483a0d4296789a
Pascal <pascal.kiessler@haw-hamburg.de>
2020-06-14 13:10:38 +0200

    Adjazenzmatrix soweit fertig

--- Aufgabe_4/src/Adjazenzmatrix.java
+++ Aufgabe_4/src/Adjazenzmatrix.java
@@ -5,13 +5,13 @@ public class Adjazenzmatrix <T> implements Graph {
     private boolean [][] adjazenzmatrix;
     private final int ARRAYSIZE;
     private int anzahlKnoten;
-    private T [] knotenarray;
+    private KnotenGraph [] knotenarray;
 
     public Adjazenzmatrix () {
         ARRAYSIZE = 10;
         adjazenzmatrix = new boolean [ARRAYSIZE][ARRAYSIZE];
         anzahlKnoten = 0;
-          knotenarray = (T[]) new Comparable[ARRAYSIZE];
+          knotenarray = new KnotenGraph[ARRAYSIZE];
     }
 
 
@@ -20,7 +20,8 @@ public class Adjazenzmatrix <T> implements Graph {
     @Override
     public void knotenEinfuegen(Object data) {
         if(anzahlKnoten < knotenarray.length) { //sorgt dafür, dass keine Fehlermeldung erscheint, wenn zu viele Knoten erzeugt werden
-            knotenarray[anzahlKnoten] = (T) new KnotenGraph (data); //fügt den Knoten an das einfache Feld der Knoten an
+            KnotenGraph neuerKnoten = new KnotenGraph (data);
+            knotenarray[anzahlKnoten] = neuerKnoten; //fügt den Knoten an das einfache Feld der Knoten an
             adjazenzmatrix[anzahlKnoten][anzahlKnoten] = false; //fügt in der Matrix -1 ein, da hier keine Kante entstehen kann
             anzahlKnoten++;
         } else {
@@ -31,11 +32,11 @@ public class Adjazenzmatrix <T> implements Graph {
 
     @Override
     public void kanteEinfuegen(int knoten1, int knoten2) {
-        if(knotenVorhanden(knoten1)) //kontrolliert mit der Methode KnotenNummer, ob der Startknoten der Kante vorhanden ist
+        if (!knotenVorhanden(knoten1)) //kontrolliert mit der Methode KnotenNummer, ob der Startknoten der Kante vorhanden ist
         {
             System.out.println("Der Startknoten existiert nicht.");
         } else {
-            if(knotenVorhanden(knoten2)) //kontrolliert mit der Methode KnotenNummer, ob der Endknoten der Kante voranden ist
+            if (!knotenVorhanden(knoten2)) //kontrolliert mit der Methode KnotenNummer, ob der Endknoten der Kante voranden ist
             {
                 System.out.println("Der Endknoten existiert nicht.");
             } else {
@@ -65,11 +66,10 @@ public class Adjazenzmatrix <T> implements Graph {
 
     @Override
     public int traversieren(int knoten1) {
-        int knoten = 0;
         int result = 0;
 
-        for (int i = 0; i <adjazenzmatrix.length; i++){
-            if (adjazenzmatrix[knoten][i]){
+        for (int i = 0; i < anzahlKnoten; i++){
+            if (adjazenzmatrix[knoten1][i]){
                 result++;
             }
         }
@@ -94,37 +94,37 @@ public class Adjazenzmatrix <T> implements Graph {
 
 
     public void allesAusgeben (){
-        System.out.print("   ");
+        System.out.print("\t\t");
         for(int y = 0; y < anzahlKnoten; y++) //druckt die erste Zeile (alle Knoten in deren Reihenfolge)
         {
             System.out.print(gibKnoten(y).getDaten());
-            System.out.print("|");
+            System.out.print(" | \t");
         }
         for(int i = 0; i < anzahlKnoten; i++) //druckt die übrigen Zeilen untereinander
         {
             System.out.println(" ");
 
             System.out.print(gibKnoten(i).getDaten());
-            System.out.print("|");
+            System.out.print(" | ");
             for(int z = 0; z < anzahlKnoten; z++) //druckt die entsprechende Teile die die Gewichtungen von Knoten[i] anzeigt
             {
                 if(adjazenzmatrix[i][z] != false) //kontrolliert, dass keine Kanten erzeugt werden, wo keine erzeugt werden können
                 {
                     if(adjazenzmatrix[i][z] == true) //erstetzt alle Zellen in denen 0 steht durch " "
                     {
-                        System.out.print("  ");
-                        System.out.print("|");
+                        System.out.print(" true ");
+                        System.out.print(" | ");
                     }
                     else
                     {
                         System.out.print(" ");
-                        System.out.print(adjazenzmatrix[i][z]);
-                        System.out.print("|");
+                        System.out.print(" " + adjazenzmatrix[i][z] + " ");
+                        System.out.print(" | ");
                     }
                 }
                 else
                 {
-                    System.out.print(adjazenzmatrix[i][z]);
+                    System.out.print(" " + adjazenzmatrix[i][z] + " ");
 
                     System.out.print("|");
                 }
